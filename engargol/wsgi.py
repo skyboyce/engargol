26826b809f4561fa7ee4dbdc7c9300b123e8f6bb
skyboyce <skyboyce80@gmail.com>
2021-10-29 19:36:22 +0330

    initial commit

---
+++ engargol/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for engargol project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'engargol.settings')
+
+application = get_wsgi_application()
