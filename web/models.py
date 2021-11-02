4159c9aec2141a1597154d6702acccf3f7cc8ece
martin <skyboyce80@gmail.com>
2021-11-02 08:42:17 +0330

    initioanl commit

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+ #Create your models here.
+"""class Expense(models.Model):
+    text = models.CharField()
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+"""
