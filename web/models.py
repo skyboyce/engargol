ba028fc0b5724d1efd6f428abd257d80550fbef4
martin <skyboyce80@gmail.com>
2021-11-06 16:34:13 +0330

    submit/income and submit/income web services

--- web/models.py
+++ web/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
  #Create your models here.
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __str__(self):
+        return "{}_Token".format(self.user)
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
@@ -9,8 +16,6 @@ class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return "{},{},{}".format(self.text, self.date, self.amount)
-
-
 
 class Income(models.Model):
     text = models.CharField(max_length=255)
